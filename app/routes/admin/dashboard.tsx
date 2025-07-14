e3171427d1759ee687971cfc3ed284f5484843ea
NathanNhan <44769901+NathanNhan@users.noreply.github.com>
2025-07-14 13:01:51 +0700

    Implement Routing, Dashboard Layout & UI, Auth Functionality

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,9 +1,71 @@
+import { Header, StatCard, TripCard } from "components"
+import { allTrips, dashboardStats, user } from "~/constants";
+
+
 
 
 const dashboard = () => {
+  
+
+  const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } = dashboardStats;
   return (
-    <div>dashboard Page</div>
-  )
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"}`}
+        description="Track activity, trends and popular destinations in real time"
+      />
+      Dashboard Page Contents
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatCard
+            headerTitle="Total Users"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatCard
+            headerTitle="Total Trips"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatCard
+            headerTitle="Active Users"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">Created Trips</h1>
+        <div className="trip-grid">
+          {allTrips
+            .slice(0, 4)
+            .map(
+              ({
+                id,
+                name,
+                imageUrls,
+                itinerary,
+                tags,
+                travelStyle,
+                estimatedPrice,
+              }) => (
+                <TripCard key={id}
+                   id={id.toString()}
+                   name = {name}
+                   imageUrl={imageUrls[0]}
+                   location={itinerary?.[0]?.location ?? ''}
+                   tags = {tags} 
+                   price = {estimatedPrice}
+                />
+              )
+            )}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default dashboard
